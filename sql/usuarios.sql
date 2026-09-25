cbb6ff81c2ff567eede584a877260e4c1ddaa65d
KauaEduardo08 <kauabrito637@gmail.com>
2026-09-25 11:54:50 -0300

    Ou

--- sql/usuarios.sql
+++ sql/usuarios.sql
@@ -2,5 +2,6 @@ USE valistoque_testes;
 
 INSERT INTO usuarios (nome, email, cpf, senha, tipo)
 VALUES
+('ana', 'ana@email.com', '12345647', '1234', 'funcionario'),
 ('Administrador', 'admin@valistoque.com', '111.111.111-11', 'admin123', 'administrador'),
 ('Funcionario', 'funcionario@valistoque.com', '222.222.222-22', 'func123', 'funcionario');
